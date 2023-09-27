1339be139241164c4cbb1ae74183b335db5a7c56
Lathika Sunder <lathikasunder11@gmail.com>
2023-09-27 22:23:40 +0530

    ADD - id field and main page scrap

--- scraper.py
+++ scraper.py
@@ -3,61 +3,54 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import pandas as pd
 import time
-import csv  # Import the csv module
 
-url1 = 'https://www.linkedin.com/jobs/search?keywords=Marketing%20Data%20Analyst&location=Berlin%2C%20Berlin%2C%20Germany&geoId=106967730&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=1'
-
-s = Service("chromedriver.exe")
-
-driver = webdriver.Chrome(service=s)
-driver.get(url1)
-driver.implicitly_wait(10)
-y = driver.find_elements(By.CLASS_NAME, 'results-context-header__job-count')[0].text
-print(y)
-
-n = pd.to_numeric(y)
-
-i = 2
-while i <= int(2) + 1:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    i = i + 1
-
-    try:
-        send = driver.find_element(By.XPATH, "//button[@aria-label='Load more results']")
-        driver.execute_script("arguments[0].click();", send)
+# Function to scrape LinkedIn job listings
+def scrape_linkedin_jobs(url, num_jobs_to_scrape):
+    s = Service("chromedriver.exe")
+    driver = webdriver.Chrome(service=s)
+    driver.get(url)
+    driver.implicitly_wait(10)
+    
+    # Scroll down to load more job listings
+    def scroll_to_load_more():
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(3)
+    
+    job_data = {
+        'id': [],
+        'company': [],
+        'title': [],
+    }
+    
+    while len(job_data['id']) < num_jobs_to_scrape:
+        scroll_to_load_more()
+        
+        job_ids = driver.find_elements(By.CLASS_NAME, 'base-card')
+        for i, job in enumerate(job_ids):
+            if len(job_data['id']) >= num_jobs_to_scrape:
+                break
+            
+            job_data['id'].append(len(job_data['id']) + 1)
+            company = job.find_element(By.CLASS_NAME, 'base-search-card__subtitle').text
+            title = job.find_element(By.CLASS_NAME, 'base-search-card__title').text
+            
+            job_data['company'].append(company)
+            job_data['title'].append(title)
+            
+        try:
+            load_more_button = driver.find_element(By.XPATH, "//button[@aria-label='Load more results']")
+            driver.execute_script("arguments[0].click();", load_more_button)
+            time.sleep(3)
+        except:
+            break
+    
+    df = pd.DataFrame(job_data)
+    df.to_csv('linkedin.csv', index=False)  # Save to CSV
+    df.to_json('linkedin.json', orient='records')  # Save to JSON
 
-    except:
-        pass
-        time.sleep(5)
+# Define the URL and number of job listings to scrape
+url_to_scrape = 'https://www.linkedin.com/jobs/search'
+num_jobs_to_scrape = 2000  # Change this number to the desired number of job listings
 
-companyname = []
-titlename = []
-
-try:
-    for i in range(n):
-        company = driver.find_elements(By.CLASS_NAME, 'base-search-card__subtitle')[i].text
-        companyname.append(company)
-        # Extract job titles from the 'h1' tag
-        title = driver.find_elements(By.CLASS_NAME, 'base-search-card__title')[i].text
-        titlename.append(title)
-
-except IndexError:
-    print("no")
-
-# Create a DataFrame with company and title columns
-data = {'company': companyname, 'title': titlename}
-df = pd.DataFrame(data)
-
-# Save the DataFrame to 'linkedin.csv' file
-df.to_csv('linkedin.csv', index=False)  # Use index=False to exclude the index column
-
-# Read the CSV file
-df = pd.read_csv('linkedin.csv')
-
-# Convert the DataFrame to JSON
-json_data = df.to_json(orient='records')
-
-# Save the JSON data to 'linkedin.json' file
-with open('linkedin.json', 'w') as json_file:
-    json_file.write(json_data)
+# Call the scraping function
+scrape_linkedin_jobs(url_to_scrape, num_jobs_to_scrape)
