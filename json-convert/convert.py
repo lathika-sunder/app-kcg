58ab35f5b283898c07615a2875f5b316b558d4fd
Lathika Sunder <lathikasunder11@gmail.com>
2023-09-30 01:51:38 +0530

    Devops ok - Frontend position issue

--- json-convert/convert.py
+++ json-convert/convert.py
@@ -9,16 +9,27 @@ initial_nodes = []
 for control in data["mockup"]["controls"]["control"]:
     if "children" in control and "controls" in control["children"] and "control" in control["children"]["controls"]:
         children_controls = control["children"]["controls"]["control"]
+        # Calculate the total width required for all nodes in children_controls
+        total_width = sum([len(child.get("properties", {}).get("text", "")) for child in children_controls])
+        # Calculate the initial X-coordinate for the first node
+        start_x = 900 - total_width * 10  # Adjust the spacing between characters as needed (10 units used here)
+
         for index, child in enumerate(children_controls):
             if "properties" in child and "text" in child["properties"]:
                 text = child["properties"]["text"]
+                x=child["x"]
+                y=child["y"]
                 node_id = f"{control['ID']}-{index}"
+                node_width = len(text) * 10  # Assuming each character occupies 10 units of width
+                # node_x = start_x + (node_width / 2)  # Center the node text
                 node = {
                     "id": node_id,
                     "data": {"label": text},
-                    "position": {"x": 900, "y": 100 + index * 100}  # Adjust y position as needed
+                    "position": {"x": x, "y": y}  # Adjust y position as needed
                 }
                 initial_nodes.append(node)
+                # Update start_x for the next node
+                start_x += node_width
 
 # Create nodes JSON format
 nodes_data = initial_nodes
